484ec2c6c8b0d77cc5130f27ace6760cfc4580de
muh-lippe <allifqalbiy@apps.ipb.ac.id>
2025-04-15 12:44:47 +0700

    final-final

--- setup.sh
+++ setup.sh
@@ -5,50 +5,56 @@ touch CMakeLists.txt
 
 
 # Buat folder data dan file di dalamnya
-mkdir -p data
+#mkdir -p data
+mkdir -p src
+mkdir -p include
+mkdir -p tests
 
+# Buat subfolder include
+mkdir -p include/data_structures
+mkdir -p include/models
 
-# Buat folder src dan subfoldernya
-mkdir -p src/core
-mkdir -p src/models
-mkdir -p src/auth
-mkdir -p src/menus
+# Buat file di dalam include
+touch include/utils.h
+
+# Buat file di dalam include/data_structures
+touch include/data_structures/queue.h
+touch include/data_structures/stack.h
+touch include/data_structures/algorithms.h
+
+# Buat file di dalam include/models
+touch include/models/user.h
+touch include/models/workshop.h
+touch include/models/registration.h
+
+# Buat subfolder src
+mkdir -p src/database
 mkdir -p src/services
+mkdir -p src/menus
+mkdir -p src/utils
 
 # Buat file di dalam src
 touch src/main.cpp
 
-# Buat file di dalam src/core
-touch src/core/database.h
-touch src/core/database.cpp
-touch src/core/utils.h
-touch src/core/utils.cpp
-
-# Buat file di dalam src/models
-touch src/models/user.h
-touch src/models/admin.h
-touch src/models/participant.h
-touch src/models/workshop.h
-touch src/models/training.h
-touch src/models/registration.h
-
-# Buat file di dalam src/auth
-touch src/auth/auth.h
-touch src/auth/auth.cpp
-
-# Buat file di dalam src/menus
-touch src/menus/menu.h
-touch src/menus/main_menu.h
-touch src/menus/admin_menu.h
-touch src/menus/participant_menu.h
+# Buat file di dalam src/database
+touch src/database/database.h
+touch src/database/database.cpp
 
 # Buat file di dalam src/services
 touch src/services/admin_service.h
 touch src/services/participant_service.h
-touch src/services/workshop_service.h
+touch src/services/auth_service.h
 
-# Buat folder tests dan file di dalamnya
-mkdir -p tests
-touch tests/test_main.cpp
+# Buat file di dalam src/menus
+touch src/menus/admin_menu.h
+touch src/menus/participant_menu.h
+
+# Buat file di dalam src/utils
+touch src/utils/hashing.cpp
+touch src/utils/validation.cpp
+
+# Buat file di dalam tests
+touch tests/test_queue.cpp
+touch tests/test_sorting.cpp
 
 echo "Struktur direktori dan file telah dibuat di folder kuda."
